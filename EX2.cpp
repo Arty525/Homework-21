0a79e1473d5a2e667627134af8dd0bd10fd6e344
Andrey <89473462+Arty525@users.noreply.github.com>
2022-07-07 19:28:29 +0300

    Add files via upload

--- EX2.cpp
+++ EX2.cpp
@@ -70,8 +70,8 @@ int main() {
         land[i].number = i + 1;
         std::cout << "Landscape #" << land[i].number << std::endl;
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Участок №";
-        file << buff << land[i].number << std::endl;
+        char l[] = "Участок №";
+        file << l << land[i].number << std::endl;
         file.close();
         do {
             std::cout << "Enter number of houses on this landscape: ";
@@ -79,15 +79,15 @@ int main() {
             if (land[i].houses <= 0) std::cout << "The number of houses must be greater than zero";
         } while (land[i].houses <= 0);
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Количество жилых домов: ";
-        file << buff << land[i].houses << std::endl;
+        char h[] = "Количество жилых домов: ";
+        file << h << land[i].houses << std::endl;
         file.close();
         for (int j = 0; j < land[i].houses; ++j) {
             land[i].h.push_back(house());
             std::cout << "House #" << j + 1 << std::endl;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Дом №";
-            file << buff << j + 1 << std::endl;
+            char hn[] = "Дом №";
+            file << hn << j + 1 << std::endl;
             file.close();
             do {
                 std::cout << "Enter number of floors: ";
@@ -95,28 +95,28 @@ int main() {
                 if (land[i].h[j].floors <= 0) std::cout << "The number of floors must be greater than zero";
             } while (land[i].h[j].floors <= 0);
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Количество этажей: ";
-            file << buff << land[i].h[j].floors << std::endl;
+            char f[] = "Количество этажей: ";
+            file << f << land[i].h[j].floors << std::endl;
             file.close();
             std::cout << "Enter total square of this house: ";
             std::cin >> land[i].h[j].square;
             land[i].squareOfBuildings += land[i].h[j].square;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Площадь дома: ";
-            file << buff << land[i].h[j].square << std::endl;
+            char hs[] = "Площадь дома: ";
+            file << hs << land[i].h[j].square << std::endl;
             file.close();
             std::cout << "Is there a furnace in this house? (1/0): ";
             std::cin >> land[i].h[j].furnace;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Печь: ";
-            file << buff << land[i].h[j].furnace << std::endl;
+            char hf[] = "Печь: ";
+            file << hf << land[i].h[j].furnace << std::endl;
             file.close();
             for (int k = 0; k < land[i].h[j].floors; ++k) {
                 land[i].h[j].f.push_back(flr());
                 std::cout << "Floor #" << k + 1 << std::endl;
                 file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                char buff[] = "Этаж №";
-                file << buff << k + 1 << std::endl;
+                char fn[] = "Этаж №";
+                file << fn << k + 1 << std::endl;
                 file.close();
                 do {
                     std::cout << "Enter height of this floor: ";
@@ -124,8 +124,8 @@ int main() {
                     if (land[i].h[j].f[k].height <= 0) std::cout << "Height must be greater than zero";
                 } while (land[i].h[j].f[k].height <= 0);
                 file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                char buff[] = "Высота этажа: ";
-                file << buff << land[i].h[j].f[k].height << std::endl;
+                char fh[] = "Высота этажа: ";
+                file << fh << land[i].h[j].f[k].height << std::endl;
                 file.close();
                 do {
                     std::cout << "Enter number of rooms on this floor: ";
@@ -133,8 +133,8 @@ int main() {
                     if (land[i].h[j].floors <= 0) std::cout << "The number of rooms must be greater than zero";
                 } while (land[i].h[j].f[k].rooms <= 0);
                 file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                char buff[] = "Количество комнат на этаже: ";
-                file << buff << land[i].h[j].f[k].rooms << std::endl;
+                char fr[] = "Количество комнат на этаже: ";
+                file << fr << land[i].h[j].f[k].rooms << std::endl;
                 file.close();
                 int r = land[i].h[j].f[k].rooms;
                 do {
@@ -143,8 +143,8 @@ int main() {
                         std::cout << "Enter number of lounge: ";
                         std::cin >> land[i].h[j].f[k].lounge;
                         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                        char buff[] = "Гостинная: ";
-                        file << buff << land[i].h[j].f[k].lounge << std::endl;
+                        char lounge[] = "Гостинная: ";
+                        file << lounge << land[i].h[j].f[k].lounge << std::endl;
                         file.close();
                         r -= land[i].h[j].f[k].lounge;
                     }
@@ -153,8 +153,8 @@ int main() {
                         std::cout << "Enter number of kid's room: ";
                         std::cin >> land[i].h[j].f[k].kids;
                         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                        char buff[] = "Детская: ";
-                        file << buff << land[i].h[j].f[k].kids << std::endl;
+                        char kids[] = "Детская: ";
+                        file << kids << land[i].h[j].f[k].kids << std::endl;
                         file.close();
                         r -= land[i].h[j].f[k].kids;
                     }
@@ -163,8 +163,8 @@ int main() {
                         std::cout << "Enter number of kitchen: ";
                         std::cin >> land[i].h[j].f[k].kitchen;
                         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                        char buff[] = "Кухня: ";
-                        file << buff << land[i].h[j].f[k].kitchen << std::endl;
+                        char kitchen[] = "Кухня: ";
+                        file << kitchen << land[i].h[j].f[k].kitchen << std::endl;
                         file.close();
                         r -= land[i].h[j].f[k].kitchen;
                     }
@@ -173,8 +173,8 @@ int main() {
                         std::cout << "Enter number of bedroom: ";
                         std::cin >> land[i].h[j].f[k].bedroom;
                         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-                        char buff[] = "Спальня: ";
-                        file << buff << land[i].h[j].f[k].bedroom << std::endl;
+                        char bedroom[] = "Спальня: ";
+                        file << bedroom << land[i].h[j].f[k].bedroom << std::endl;
                         file.close();
                         r -= land[i].h[j].f[k].bedroom;
                     }
@@ -186,27 +186,27 @@ int main() {
         std::cout << "Enter number of sauna on this landscape: ";
         std::cin >> land[i].sauna;
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Количество бань: ";
-        file << buff << land[i].sauna << std::endl;
+        char saunas[] = "Количество бань: ";
+        file << saunas << land[i].sauna << std::endl;
         file.close();
         for (int j = 0; j < land[i].sauna; ++j) {
             land[i].s.push_back(sauna());
             std::cout << "Sauna #" << j + 1 << std::endl;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Баня №";
-            file << buff << j + 1 << std::endl;
+            char saunaNumber[] = "Баня №";
+            file << saunaNumber << j + 1 << std::endl;
             file.close();
             std::cout << "Is there a furnace with tube in this sauna? (1/0): ";
             std::cin >> land[i].s[j].furnace;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Печка в бане: ";
-            file << buff << land[i].s[j].furnace << std::endl;
+            char saunaFurnace[] = "Печка в бане: ";
+            file << saunaFurnace << land[i].s[j].furnace << std::endl;
             file.close();
             std::cout << "Enter total square of this sauna: ";
             std::cin >> land[i].s[j].square;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Площадь бани: ";
-            file << buff << land[i].s[j].square << std::endl;
+            char saunaSquare[] = "Площадь бани: ";
+            file << saunaSquare << land[i].s[j].square << std::endl;
             file.close();
             land[i].squareOfBuildings += land[i].s[j].square;
         }
@@ -214,21 +214,21 @@ int main() {
         std::cout << "Enter number of garage on this landscape: ";
         std::cin >> land[i].garage;
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Количество гаражей: ";
-        file << buff << land[i].garage << std::endl;
+        char garages[] = "Количество гаражей: ";
+        file << garages << land[i].garage << std::endl;
         file.close();
         for (int j = 0; j < land[i].garage; ++j) {
             land[i].g.push_back(garage());
             std::cout << "Garage #" << j + 1 << std::endl;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Гараж №";
-            file << buff << j + 1 << std::endl;
+            char garageNumber[] = "Гараж №";
+            file << garageNumber << j + 1 << std::endl;
             file.close();
             std::cout << "Enter total square of this garage: ";
             std::cin >> land[i].g[j].square;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Площадь гаража: ";
-            file << buff << land[i].garage << std::endl;
+            char garageSquare[] = "Площадь гаража: ";
+            file << garageSquare << land[i].garage << std::endl;
             file.close();
             land[i].squareOfBuildings += land[i].g[j].square;
         }
@@ -236,36 +236,36 @@ int main() {
         std::cout << "Enter number of barn on this landscape: ";
         std::cin >> land[i].barn;
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Количество сараев: ";
-        file << buff << land[i].barn << std::endl;
+        char barns[] = "Количество сараев: ";
+        file << barns << land[i].barn << std::endl;
         file.close();
         for (int j = 0; j < land[i].barn; ++j) {
             land[i].b.push_back(barn());
             std::cout << "Barn #" << j + 1 << std::endl;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Сарай №";
-            file << buff << j + 1 << std::endl;
+            char barnNumber[] = "Сарай №";
+            file << barnNumber << j + 1 << std::endl;
             file.close();
             std::cout << "Enter total square of this barn: ";
             std::cin >> land[i].b[j].square;
             file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-            char buff[] = "Площадь сарая: ";
-            file << buff << land[i].b[j].square << std::endl;
+            char barnSquare[] = "Площадь сарая: ";
+            file << barnSquare << land[i].b[j].square << std::endl;
             file.close();
             land[i].squareOfBuildings += land[i].b[j].square;
         }
 
         totalSquareOfBuildings += land[i].squareOfBuildings;
         file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-        char buff[] = "Общая площадь построек на участке: ";
-        file << buff << land[i].squareOfBuildings << std::endl;
+        char landbs[] = "Общая площадь построек на участке: ";
+        file << landbs << land[i].squareOfBuildings << std::endl;
         file.close();
     }
 
     std::cout << "Total square of all buildings: " << totalSquareOfBuildings << std::endl;
     file.open("C:\\Users\\Public\\Documents\\village.txt", std::ios::app);
-    char buff[] = "Общая площадь построек в поселке: ";
-    file << buff << totalSquareOfBuildings << std::endl;
+    char totalSquare[] = "Общая площадь построек в поселке: ";
+    file << totalSquare << totalSquareOfBuildings << std::endl;
     file.close();
 
     return 0;
