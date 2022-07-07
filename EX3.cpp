8f620ed2fd9b733c56bf5d5485fcc81d1367f537
Andrey <89473462+Arty525@users.noreply.github.com>
2022-07-07 20:45:08 +0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+struct vector {
+	char b[40];
+};
+
+int main() {
+	vector a[40];
+
+	std::cout << "Enter name of vector: ";
+	char vname;
+	std::cin >> vname;
+	std::cout << "Enter X coordinate: ";
+	int x;
+	std::cin >> x;
+	std::cout << "Enter Y coordinate: ";
+	int y;
+	std::cin >> y;
+
+	a[x].b[y] = vname += 'x';
+
+}
